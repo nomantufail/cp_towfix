013a295f25c9c58e68c8bce66a0c2f257688a21a
waqas ahmad <waqasahmad.codingpixel@gmail.com>
2016-11-22 23:10:04 -0800

    email blade

---
+++ resources/views/mail/service-reminder.blade.php
@@ -0,0 +1,32 @@
+<html>
+
+
+/**
+ * Created by PhpStorm.
+ * User: Mobeen
+ * Date: 11/22/2016
+ * Time: 10:38 PM
+ */
+
+
+<body>
+<h3>
+    Vehicle Service Reminder Of Your Vehicle
+</h3>
+
+<p>
+    Tow Fix is Australia's only mobile caravan, trailer and horse float service and repair franchise. We service all types of caravans, pop-tops and campers, all sorts of trailers including boat, car, work, jet ski and horse floats....in fact anything you can tow! And we do it all at your home, caravan park or storage yard!
+    So next time you require a service or repair on your van, trailer or float, don't have the hassle of hooking up and taking it to a workshop….let our workshop come to you!
+
+
+
+</p>
+
+<span> Franchises Now Available</span>
+
+
+<img src="{{url('/')}}/images/maillogo.png" alt="">
+</body>
+
+
+</html>
